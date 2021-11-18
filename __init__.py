20f343e2f7bb0ad8fe5e4ea65a4ff10a794ebfc4
x220 <dlcmaldmlzldxkdlrj@gmial.com>
2021-11-18 18:21:37 +0900

    refactor: wipe comments

--- __init__.py
+++ __init__.py
@@ -15,17 +15,12 @@ PLATFORMS: list[str] = [
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up sihas_canary from a config entry."""
-    # TODO Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
-
+    # NOTE: how about checking supported type at here?
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)

163b3aa7f558f75fe97c202e8f6ac884b60b51dd
x220 <dlcmaldmlzldxkdlrj@gmial.com>
2021-11-24 10:17:06 +0900

    refactor: ad log msg when setup and unload entry

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 """The sihas_canary integration."""
 from __future__ import annotations
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[str] = [
     "climate",
@@ -14,12 +18,14 @@ PLATFORMS: list[str] = [
 ]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # NOTE: how about checking supported type at here?
+    _LOGGER.info(f"entry setuped: {entry}")
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    _LOGGER.info(f"entry unloadded: {entry}")
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     return unload_ok

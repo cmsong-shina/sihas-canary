2b075c3012b320f2b2120de4ae681f7fc8539314
x220 <dlcmaldmlzldxkdlrj@gmial.com>
2021-12-14 14:38:09 +0900

    feat: SDM 색온도/밝기 조절 기능 추가

--- light.py
+++ light.py
@@ -1,15 +1,20 @@
 """Platform for light integration."""
 from __future__ import annotations
+
 from datetime import timedelta
+from typing import Any, Dict, Final, List, cast
 
-from typing import List
-
-from homeassistant.components.light import COLOR_MODE_BRIGHTNESS, COLOR_MODE_COLOR_TEMP, LightEntity
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ATTR_COLOR_TEMP,
+    COLOR_MODE_BRIGHTNESS,
+    COLOR_MODE_COLOR_TEMP,
+    LightEntity,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import (
     CONF_CFG,
@@ -153,33 +158,71 @@ class SdmVirtualLight(LightEntity):
 
         self._proxy = sdm
         self._attr_available = self._proxy._attr_available
-        self._state = None
-        self._number_of_switch = number_of_switch
+        """
+        self._state {
+            "onoff": bool,
+            ATTR_BRIGHTNESS: int(0~255),
+            ATTR_COLOR_TEMP: int(154~500),
+        }
+        """
+        self._state: Dict[str, Any] = {}
+        self._number_of_switch: Final = number_of_switch
         self._attr_unique_id = uid
         self._attr_name = f"{name} #{number_of_switch}" if name else uid
 
         # set Supported Color Modes
-        scm = {COLOR_MODE_BRIGHTNESS}
+        self._attr_supported_color_modes = {COLOR_MODE_BRIGHTNESS}
         if self._proxy.is_colortemperature_changable:
-            scm.add(COLOR_MODE_COLOR_TEMP)
-        self._attr_supported_color_modes = scm
+            self._attr_supported_color_modes.add(COLOR_MODE_COLOR_TEMP)
 
-    # TODO: 레지스터 맞게 설정, 메서드 작성
+    @property
+    def register_offset(self):
+        return self._number_of_switch * 2
+
     @property
     def is_on(self):
-        return self._state
+        return self._state["onoff"]
+
+    @property
+    def brightness(self):
+        return self._state[ATTR_BRIGHTNESS]
+
+    @property
+    def color_temp(self):
+        return self._state[ATTR_COLOR_TEMP]
 
     def update(self):
         self._proxy.update()
-        self._state = self._proxy.registers[self._number_of_switch] == 1
+        self._state["onoff"] = self._proxy.registers[self.register_offset] != 0
+        self._state[ATTR_BRIGHTNESS] = int(self._proxy.registers[self.register_offset] / 100 * 255)
+        color_temp = self._proxy.registers[self.register_offset + 1]
+        self._state[ATTR_COLOR_TEMP] = int(346 - (color_temp / 100 * 346)) + 154
         self._attr_available = self._proxy._attr_available
 
     def turn_on(self, **kwargs):
-        self._set_switch(True)
+        opt = {ATTR_BRIGHTNESS: 101}
+        if ATTR_BRIGHTNESS in kwargs:
+            brightness = cast(int, kwargs.get(ATTR_BRIGHTNESS))
+            opt[ATTR_BRIGHTNESS] = int(brightness / 255 * 100)
+
+        if ATTR_COLOR_TEMP in kwargs:
+            # 색온도는 154-500의 범위 안에서 변환한다
+            color_temp = cast(int, kwargs.get(ATTR_COLOR_TEMP))
+            opt[ATTR_COLOR_TEMP] = int((346 - (color_temp - 154)) / 346 * 100)
+
+        self._set_switch(opt)
 
     def turn_off(self, **kwargs):
-        self._set_switch(False)
+        self._set_switch({ATTR_BRIGHTNESS: 0})
 
-    def _set_switch(self, onoff: bool):
-        val = 1 if onoff else 0
-        self._proxy.command(self._number_of_switch, val)
+    def _set_switch(self, option: Dict):
+        """
+        option {
+            ATTR_BRIGHTNESS?: int, // 밝기, 0~100
+            ATTR_COLOR_TEMP?: int, // 색온도
+        }
+        """
+        if ATTR_BRIGHTNESS in option:
+            self._proxy.command(self.register_offset, option[ATTR_BRIGHTNESS])
+        if ATTR_COLOR_TEMP in option:
+            self._proxy.command(self.register_offset + 1, option[ATTR_COLOR_TEMP])

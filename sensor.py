b79426635197938c56cf981fd63adceaf8e53407
Murian <y2k001hj@gmail.com>
2023-02-02 16:52:08 +0900

    Update sensor.py
    https://cafe.naver.com/koreassistant/10588
    
    HA 네이버 카페의 우기네 님이 수정한 버전을 제가 다시 조금 더 수정한 버전입니다.
    
    HA 네이버 카페의 많은 사람들이 이 파일로 시하스 컴포넌트를 이용하고 있습니다.
    
    이 내용을 참조해서 기능을 업데이트 해주시면 좋을 것 같습니다.
    
    이 컴포넌트를 HACS에 등록하는건 제 능력 바깥이고 시하스에서 업데이트 해주시면 좋을 것 같습니다.

--- sensor.py
+++ sensor.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
+import logging
 from datetime import timedelta
 from typing import Callable, Dict, List, Optional
+import asyncio
 
 from homeassistant.components.sensor import (
     STATE_CLASS_MEASUREMENT,
@@ -16,15 +18,26 @@ from homeassistant.const import (
     CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
     CONCENTRATION_PARTS_PER_BILLION,
     CONCENTRATION_PARTS_PER_MILLION,
-    ENERGY_WATT_HOUR,
+    ENERGY_KILO_WATT_HOUR,
     LIGHT_LUX,
     PERCENTAGE,
     POWER_WATT,
     TEMP_CELSIUS,
+    ELECTRIC_POTENTIAL_VOLT,
+    ELECTRIC_CURRENT_AMPERE,
+    FREQUENCY_HERTZ,
+    ENERGY_WATT_HOUR,
+    DEVICE_CLASS_POWER,
+    DEVICE_CLASS_VOLTAGE,
+    DEVICE_CLASS_CURRENT,
+    DEVICE_CLASS_POWER_FACTOR,
+    DEVICE_CLASS_FREQUENCY,
+    DEVICE_CLASS_ENERGY
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from typing_extensions import Final
 
 from .const import (
@@ -36,10 +49,10 @@ from .const import (
     ICON_POWER_METER,
     SIHAS_PLATFORM_SCHEMA,
 )
-from .sihas_base import SihasProxy
+from .sihas_base import SihasEntity, SihasProxy
 from .util import register_put_u32
 
-SCAN_INTERVAL = timedelta(seconds=5)
+SCAN_INTERVAL = timedelta(seconds=10)
 
 PARALLEL_UPDATES = DEFAULT_PARALLEL_UPDATES
 PLATFORM_SCHEMA = SIHAS_PLATFORM_SCHEMA
@@ -96,64 +109,120 @@ AQM_GENERIC_SENSOR_DEFINE: Final = {
     },
 }
 
-PMM_KEY_POWER: Final = "power"
-PMM_KEY_THIS_MONTH_ENERGY: Final = "this_month_energy"
-PMM_KEY_THIS_DAY_ENERGY: Final = "this_day_energy"
-PMM_KEY_TOTAL: Final = "total_energy"
-
-
-@dataclass
-class PmmConfig:
-    nuom: str
-    value_handler: Callable
-    device_class: SensorDeviceClass
-    state_class: str
-    sub_id: str
-
-
 PMM_GENERIC_SENSOR_DEFINE: Final = {
-    PMM_KEY_POWER: PmmConfig(
-        nuom=POWER_WATT,
-        value_handler=lambda r: r[2],
-        device_class=SensorDeviceClass.POWER,
-        state_class=STATE_CLASS_MEASUREMENT,
-        sub_id=PMM_KEY_POWER,
-    ),
-    PMM_KEY_THIS_MONTH_ENERGY: PmmConfig(
-        nuom=ENERGY_WATT_HOUR,
-        value_handler=lambda r: r[10] * 10,
-        device_class=SensorDeviceClass.ENERGY,
-        state_class=STATE_CLASS_TOTAL,
-        sub_id=PMM_KEY_THIS_MONTH_ENERGY,
-    ),
-    PMM_KEY_THIS_DAY_ENERGY: PmmConfig(
-        nuom=ENERGY_WATT_HOUR,
-        value_handler=lambda r: r[8] * 10,
-        device_class=SensorDeviceClass.ENERGY,
-        state_class=STATE_CLASS_TOTAL,
-        sub_id=PMM_KEY_THIS_DAY_ENERGY,
-    ),
-    PMM_KEY_TOTAL: PmmConfig(
-        nuom=ENERGY_WATT_HOUR,
-        value_handler=lambda r: register_put_u32(r[40], r[41]),
-        device_class=SensorDeviceClass.ENERGY,
-        state_class=STATE_CLASS_TOTAL_INCREASING,
-        sub_id=PMM_KEY_TOTAL,
-    ),
+    "cur_energy": {
+        "nuom": POWER_WATT,
+        "value_handler": lambda r: r[2],
+        "device_class": DEVICE_CLASS_POWER,
+        "state_class": STATE_CLASS_MEASUREMENT,
+        "sub_id": "power",
+    },
+    "this_month_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[10] / 100 + r[16] / 1000,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "this_month_energy",
+    },
+    "this_day_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[8] / 100 + r[16] / 1000,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "this_day_energy",
+    },
+    "voltage": {
+        "nuom": ELECTRIC_POTENTIAL_VOLT,
+        "value_handler": lambda r: r[0] / 10,
+        "device_class": DEVICE_CLASS_VOLTAGE,
+        "state_class": STATE_CLASS_MEASUREMENT,
+        "sub_id": "voltage",
+    },
+    "current": {
+        "nuom": ELECTRIC_CURRENT_AMPERE,
+        "value_handler": lambda r: r[1] / 100,
+        "device_class": DEVICE_CLASS_CURRENT,
+        "state_class": STATE_CLASS_MEASUREMENT,
+        "sub_id": "current",
+    },
+    "power_factor": {
+        "nuom": PERCENTAGE,
+        "value_handler": lambda r: r[3] / 10,
+        "device_class": DEVICE_CLASS_POWER_FACTOR,
+        "state_class": STATE_CLASS_MEASUREMENT,
+        "sub_id": "power_factor",
+    },
+    "frequency": {
+        "nuom": FREQUENCY_HERTZ,
+        "value_handler": lambda r: r[4] / 10,
+        "device_class": DEVICE_CLASS_FREQUENCY,
+        "state_class": STATE_CLASS_MEASUREMENT,
+        "sub_id": "frequency",
+    },
+    "total_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: (r[41] * 65536 + r[40] + r[16]) / 1000,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "total_energy",
+    },
+    "this_hour_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[6] / 100 + r[16] / 1000,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "this_hour_energy",
+    },
+    "before_hour_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[7] / 100,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "before_hour_energy",
+    },
+    "yesterday_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[9] / 100,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "yesterday_energy",
+    },
+    "last_month_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[11] / 100,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "last_month_energy",
+    },
+    "two_months_ago_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[12] / 100,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "two_months_ago_energy",
+    },
+    "this_month_forecast_energy": {
+        "nuom": ENERGY_KILO_WATT_HOUR,
+        "value_handler": lambda r: r[13] / 100,
+        "device_class": DEVICE_CLASS_ENERGY,
+        "state_class": STATE_CLASS_TOTAL,
+        "sub_id": "this_month_forecast_energy",
+    },
 }
 
-
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant, entry: ConfigEntry, async_add_devices: AddEntitiesCallback
 ) -> None:
     if entry.data[CONF_TYPE] == "PMM":
-        pmm = Pmm300(
+        device = Device("PMM300", entry)
+
+        pmm = Pmm300(device,
             ip=entry.data[CONF_IP],
             mac=entry.data[CONF_MAC],
             device_type=entry.data[CONF_TYPE],
             config=entry.data[CONF_CFG],
         )
-        async_add_entities(pmm.get_sub_entities())
+        async_add_devices(pmm.get_sub_entities())
 
     elif entry.data[CONF_TYPE] == "AQM":
         aqm = Aqm300(
@@ -162,12 +231,59 @@ async def async_setup_entry(
             device_type=entry.data[CONF_TYPE],
             config=entry.data[CONF_CFG],
         )
-        async_add_entities(aqm.get_sub_entities())
+        async_add_devices(aqm.get_sub_entities())
 
+
+class Device:
+    """Dummy roller (device for HA) for Hello World example."""
+
+    def __init__(self, name, config):
+        """Init dummy roller."""
+        self._id = f"{name}_{config.entry_id}"
+        self._name = name
+        self._callbacks = set()
+        self._loop = asyncio.get_event_loop()
+        # Reports if the roller is moving up or down.
+        # >0 is up, <0 is down. This very much just for demonstration.
+
+        # Some static information about this device
+        self.firmware_version = "1.0"
+        self.model = "PMM300"
+        self.manufacturer = "sihas"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def device_id(self):
+        """Return ID for roller."""
+        return self._id
+
+    def register_callback(self, callback):
+        """Register callback, called when Roller changes state."""
+        self._callbacks.add(callback)
+
+    def remove_callback(self, callback):
+        """Remove previously registered callback."""
+        self._callbacks.discard(callback)
+
+    # In a real implementation, this library would call it's call backs when it was
+    # notified of any state changeds for the relevant device.
+    async def publish_updates(self):
+        """Schedule call all registered callbacks."""
+        for callback in self._callbacks:
+            callback()
+
+    def publish_updates(self):
+        """Schedule call all registered callbacks."""
+        for callback in self._callbacks:
+            callback()
 
 class Pmm300(SihasProxy):
     def __init__(
         self,
+        device,
         ip: str,
         mac: str,
         device_type: str,
@@ -181,36 +297,65 @@ class Pmm300(SihasProxy):
             config=config,
         )
         self.name = name
+        self._device = device
 
     def get_sub_entities(self) -> List[Entity]:
         return [
-            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE[PMM_KEY_POWER]),
-            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE[PMM_KEY_THIS_MONTH_ENERGY]),
-            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE[PMM_KEY_THIS_DAY_ENERGY]),
-            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE[PMM_KEY_TOTAL]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["cur_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["this_month_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["this_day_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["voltage"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["current"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["power_factor"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["frequency"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["total_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["this_hour_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["before_hour_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["yesterday_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["last_month_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["two_months_ago_energy"]),
+            PmmVirtualSensor(self, PMM_GENERIC_SENSOR_DEFINE["this_month_forecast_energy"]),
         ]
 
 
 class PmmVirtualSensor(SensorEntity):
     _attr_icon = ICON_POWER_METER
 
-    def __init__(self, proxy: Pmm300, conf: PmmConfig) -> None:
+
+    def __init__(self, proxy: Pmm300, conf: Dict) -> None:
         super().__init__()
         self._proxy = proxy
+        self._device = proxy._device
         self._attr_available = self._proxy._attr_available
-        self._attr_unique_id = f"{proxy.device_type}-{proxy.mac}-{conf.sub_id}"
-        self._attr_native_unit_of_measurement = conf.nuom
-        self._attr_name = f"{proxy.name} #{conf.sub_id}" if proxy.name else self._attr_unique_id
-        self._attr_device_class = conf.device_class
-        self._attr_state_class = conf.state_class
+        self._attr_unique_id = f"{proxy.device_type}-{proxy.mac}-{conf['sub_id']}"
+        self._attr_native_unit_of_measurement = conf["nuom"]
+        self._attr_name = f"{proxy.name} #{conf['sub_id']}" if proxy.name else self._attr_unique_id
+        self._attr_device_class = conf["device_class"]
+        self._attr_state_class = conf["state_class"]
+        self._name = conf['sub_id']
 
-        self.value_handler: Callable = conf.value_handler
+        self.value_handler: Callable = conf["value_handler"]
 
     def update(self):
         self._proxy.update()
         self._attr_native_value = self.value_handler(self._proxy.registers)
         self._attr_available = self._proxy._attr_available
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def device_info(self):
+        """Information about this entity/device."""
+        return {
+            "identifiers": {("PMM300", self._device.device_id)},
+            # If desired, the name for the device could be different to the entity
+            "name": self._device.name,
+            "sw_version": self._device.firmware_version,
+            "model": self._device.model,
+            "manufacturer": self._device.manufacturer
+        }
 
 class Aqm300(SihasProxy):
     """Representation of AQM-300

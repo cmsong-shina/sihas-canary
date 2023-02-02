fea5bd6215da51c417cb55ae3143863d83a6d107
x220 <cmsong@shinasys.co.kr>
2023-02-02 13:40:42 +0900

    fix(deprecation): close #3
    See also:
    https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ PLATFORMS: list[str] = [
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # NOTE: how about checking supported type at here?
     _LOGGER.info(f"entry setuped: {entry.data}")
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 

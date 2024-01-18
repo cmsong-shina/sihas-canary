547e8f1df3374ef6209407f947a17d1d3c23f669
cmsong-shina(x220) <cmsong@shinasys.co.kr>
2024-01-18 16:16:19 +0900

    feat: add async send, scan

--- custom_components/sihas/sender.py
+++ custom_components/sihas/sender.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import socket
 import typing
@@ -47,3 +48,79 @@ def scan(data: bytes, ip: str, retry: int = 10) -> typing.Optional[str]:
             break
     _LOGGER.warn(f"failed to scan device: timeout")
     return None
+
+async def _send_message(sock: socket.socket, address: tuple[str, int], message: bytes):
+    """
+    Just async wrap of socekt.sendto()
+    """
+    sock.sendto(message, address)
+
+async def _receive_message(loop: asyncio.AbstractEventLoop, sock: socket.socket):
+    """
+    Just async wrap of eventloop.cock_recv()
+    """
+    return await loop.sock_recv(sock, 1024)
+
+async def send_async(loop: asyncio.AbstractEventLoop, data: bytes, ip: str, port: int = PORT, retry: int = 1) -> bytes:
+    """
+    # Example
+
+        ```py
+        async def main():
+            result = await scan_async(loop, "SiHAS_XXX_???".encode(), "255.255.255.255", 1)
+            print(result)
+
+        if __name__ == "__main__":
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main())
+        ```
+    """
+    raddr = (ip, port)
+
+    while retry:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                sock.settimeout(DEFAULT_TIMEOUT)
+                send_task = _send_message(sock, raddr, data)
+                receive_task = _receive_message(loop, sock)
+                _, received_message = await asyncio.gather(send_task, receive_task)
+                return received_message
+
+        except socket.timeout:
+            retry -= 1
+
+    raise socket.timeout
+
+
+async def scan_async(loop: asyncio.AbstractEventLoop, data: bytes, ip: str, retry: int = 10) -> typing.Optional[str]:
+    """
+    # Example
+
+        ```py
+        async def main():
+            result = await scan_async(loop, "SiHAS_XXX_???".encode(), "255.255.255.255", 1)
+            print(result)
+
+        if __name__ == "__main__":
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main())
+        ```
+
+    # Note
+        If we need sender address, like `recvfrom`, should concider rewrite using `create_datagram_endpoint()`.
+    """
+    raddr = (ip, 502)
+
+    while retry:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                sock.settimeout(2)
+                send_task = _send_message(sock, raddr, data)
+                receive_task = _receive_message(loop, sock)
+                _, received_message = await asyncio.gather(send_task, receive_task)
+                return received_message.decode()
+
+        except socket.timeout:
+            retry -= 1
+

513ff2fd4865fd4c5778f16e44ed47911a12cafd
cmsong-shina(x220) <cmsong@shinasys.co.kr>
2024-12-17 14:40:01 +0900

    fix(bcm): BCM이 OnOff 변경시 외출모드를 해제하도록 변경됨. 해당 부분 반영해서 모드 조작시 순서 변경

--- custom_components/sihas/climate.py
+++ custom_components/sihas/climate.py
@@ -395,8 +395,8 @@ class OutModeEntity(SelectEntity):
 
 
 class BcmHeatMode(Enum):
-    Room: Final = 0
-    Ondol: Final = 1
+    Room = 0
+    Ondol = 1
 
 
 @dataclass
@@ -453,15 +453,20 @@ class Bcm300(SihasEntity, ClimateEntity):
 
     def set_hvac_mode(self, hvac_mode: str):
         if hvac_mode == HVACMode.FAN_ONLY:
+            self.command(BCM_REG_ONOFF, 1)
+            time.sleep(0.2)
             self.command(BCM_REG_OUTMODE, 1)
-            self.command(BCM_REG_ONOFF, 1)
         elif hvac_mode == HVACMode.HEAT:
-            self.command(BCM_REG_OUTMODE, 0)
             self.command(BCM_REG_ONOFF, 1)
+            time.sleep(0.2)
+            self.command(BCM_REG_OUTMODE, 0)
+            time.sleep(0.2)
             self.command(BCM_REG_TIMERMODE, 1)
         elif hvac_mode == HVACMode.AUTO:
-            self.command(BCM_REG_OUTMODE, 0)
             self.command(BCM_REG_ONOFF, 1)
+            time.sleep(0.2)
+            self.command(BCM_REG_OUTMODE, 0)
+            time.sleep(0.2)
             self.command(BCM_REG_TIMERMODE, 0)
         elif hvac_mode == HVACMode.OFF:
             self.command(BCM_REG_ONOFF, 0)
